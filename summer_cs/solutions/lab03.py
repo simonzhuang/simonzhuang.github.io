25ff08191c62b27763787294eb32f6b83f478f17
Simon Zhuang <simonzhuang97@gmail.com>
2020-07-05 15:21:50 -0400

    post midterm update

---
+++ summer_cs/solutions/lab03.py
@@ -0,0 +1,43 @@
+def gcd(a, b):
+    """Returns the greatest common divisor of a and b.
+    Should be implemented using recursion.
+
+    >>> gcd(34, 19)
+    1
+    >>> gcd(39, 91)
+    13
+    >>> gcd(20, 30)
+    10
+    >>> gcd(40, 40)
+    40
+    """
+    if b > a:
+        a ,b = b, a
+    if a % b == 0:
+        return b
+    else:
+        return gcd(b, a % b)
+
+def hailstone(n):
+    """Print out the hailstone sequence starting at n, and return the
+    number of elements in the sequence.
+
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    """
+    length = 1
+    print(n)
+    if n != 1:
+        if n % 2 == 0:
+            length = hailstone(n // 2) + 1
+        else:
+            length = hailstone(3 * n + 1) + 1
+    return length
